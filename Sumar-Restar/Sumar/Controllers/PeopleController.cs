63ffa186b4482d2af03cbe06ec7b8e110d74fb3b
luciadanielasosa <a2019118793@estudiantes.upsa.edu.bo>
2020-08-21 21:51:24 -0400

    mi primer modelo de la clase

---
+++ Sumar-Restar/Sumar/Controllers/PeopleController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace Sumar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet]
+        public Person Listar()
+        {
+            Person person = new Person()
+            {
+                PersonId = 1,
+                Name = "Lucia Sosa"
+            };
+            return person;
+        }
+    }
+
+    internal class Person
+    {
+        public Person()
+        {
+        }
+
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Sumar-Restar/Sumar/Controllers/Person.cs
@@ -0,0 +1,5 @@
+﻿
+namespace Sumar.Controllers
+{
+   
+}
